30d4bfa34de5fa578453efd254fc0303e4025ad0
Antoine Villeret <antoine@ossia.io>
2018-05-18 14:14:45 +0200

    use OpenGLES on RPI in fullscreen mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 
 int main()
 {
+#ifdef TARGET_RASPBERRY_PI
+  ofGLESWindowSettings settings;
+  settings.windowMode = OF_FULLSCREEN;
+#else
   ofGLFWWindowSettings settings;
-
+#endif
   auto window = ofCreateWindow(settings);
   auto app = make_shared<ofApp>();
   ofRunApp(window, app);
